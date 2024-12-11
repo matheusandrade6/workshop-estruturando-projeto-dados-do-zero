839cae117a75c00ee1b13cb00aae7af8857e3ad2
devmatheus0 <92046361+devmatheus0@users.noreply.github.com>
2024-12-11 20:21:04 -0300

    corrigindo arquivo main.py

--- app/main.py
+++ app/main.py
@@ -1,2 +1,8 @@
 from pipeline.extract import read_files
+from pipeline.transform import concat_dataframes
+from pipeline.load import load_excel
 
+if __name__ == '__main__':
+    df_list = read_files('../data')
+    df = concat_dataframes(df_list)
+    load_excel(df, 'data/output', 'output')
